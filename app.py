3ddbe50f73aa0f607ae0396a18966a80be16b4e4
batra2304 <siayanamunda@gmail.com>
2020-02-05 01:54:48 +0530

    API WORK

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from bson.objectid import ObjectId
 from connection  import  *
 import json
@@ -6,24 +6,55 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/hello')
-def hello_world():
-    x = list(col.find({"_id": ObjectId("5e388991272e4c23e056d732")}))
-    oid = x[0]['_id']
-    user = col.update({"_id": ObjectId(oid)},{
+@app.route('/hello', methods= ['GET', 'POST'])
+def COLLECT_AVERAGE():
+    if request.method == 'GET':
+        x = list(col.find({"_id": ObjectId("5e388991272e4c23e056d732")}))
+        oid = x[0]['_id']
+        user = col.update({"_id": ObjectId(oid)}, {
 
-    "EXERCISE" : "PUSHUP",
-    "GENDER" : "MALE",
-    "LEVEL" : "BEGINNER",
-    "REPS" : "LOW",
-    "AGE" : "A",
-    "lift WEIGHT" : "HEAVY",
-    "HEARTRATE" : "NULL",
-    "PID" : "1"
+            "EXERCISE": "PUSHUP",
+            "GENDER": "MALE",
+            "LEVEL": "BEGINNER",
+            "REPS": "LOW",
+            "AGE": "A",
+            "lift WEIGHT": "HEAVY",
+            "HEARTRATE": "NULL",
+            "PID": "1"
+        })
+        print(user)
+        return 'hello'
+
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        x = list(col.find({"EXERCISE": data["EXERCISE"],
+            "GENDER": data["GENDER"],
+            "LEVEL": data["LEVEL"],
+            "REPS":data["REPS"],
+            "AGE": data["AGE"],
+            "lift WEIGHT": data["lift WEIGHT"]
+            }))
+
+        '''object id varible'''
+        oid = x[0]['_id']
+        if x[0]['HEARTRATE'] == "" or x[0]['HEARTRATE'] == "NULL":
+            user = col.update({"_id": ObjectId(oid)}, {
+                "EXERCISE": data["EXERCISE"],
+                "GENDER": data["GENDER"],
+                "LEVEL": data["LEVEL"],
+                "REPS":data["REPS"],
+                "AGE": data["AGE"],
+                "lift WEIGHT": data["lift WEIGHT"],
+                "HEARTRATE": data["HEARTRATE"],
+                "PID": x[0]["PID"]
                 })
-    print(user)
-    return 'hello'
+            print("DATABASE UPDATED ")
+            return "DATABASE UPDATED "
 
+        else:
+
+            print("NOT REQUIRED! ALREADY FILLED")
+            return 'NOT REQUIRED! ALREADY FILLED'
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,7 +1,3 @@
 from connection  import  *
 
 
-def hello_world():
-    user = list(col.find())
-    print(user)
-    return 'hello'
