001ff9e81b00500ffd337d29d38ec06a1c0a7bfd
batra2304 <siayanamunda@gmail.com>
2020-02-05 01:07:59 +0530

    added update query

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from bson.objectid import ObjectId
 from connection  import  *
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/hello')
 def hello_world():
-    session =[]
-    x = col.find({"_id": ObjectId("5e388991272e4c23e056d732")})
-    print(x._id)
+    x = list(col.find({"_id": ObjectId("5e388991272e4c23e056d732")}))
+    oid = x[0]['_id']
+    user = col.update({"_id": ObjectId(oid)},{
+
+    "EXERCISE" : "PUSHUP",
+    "GENDER" : "MALE",
+    "LEVEL" : "BEGINNER",
+    "REPS" : "LOW",
+    "AGE" : "A",
+    "lift WEIGHT" : "HEAVY",
+    "HEARTRATE" : "NULL",
+    "PID" : "1"
+                })
+    print(user)
     return 'hello'
-    # user = list(col.insert({"_id": session['_id'],"HEARTRATE":"NULL"}))
-    #print(list(x))
 
 
 
